cdb0cf093b843b40b248ba22117920c6177e45b8
burhan.goksel26 <burhan.goksel@popleads.com>
2024-08-20 13:38:27 +0300

    feat: add search inventory items feature

---
+++ ITInventoryManagement/ViewModel/SearchModel.cs
@@ -0,0 +1,19 @@
+﻿using ITInventoryManagement.Models;
+
+namespace ITInventoryManagement.ViewModel
+{
+    public class SearchModel
+    {
+        public string Name { get; set; }
+        public string SerialNumber { get; set; }
+        public string Model { get; set; }
+        public string Brand { get; set; }
+        public string Category { get; set; }
+        public DateTime? PurchaseDateFrom { get; set; }
+        public DateTime? PurchaseDateTo { get; set; }
+        public int? StockQuantity { get; set; }
+        public bool? IsAssigned { get; set; }
+        public bool? IsLoaned { get; set; }
+        public IEnumerable<InventoryItem> InventoryItems { get; set; }
+    }
+}
